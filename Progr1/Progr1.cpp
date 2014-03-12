962301d4e1a9cfcfb12d0fefaffa8c1a553a78da
Polleena <gp13031995@mail.ru>
2014-03-12 21:24:36 +0400

    Add all the Progr1 files

---
+++ Progr1/Progr1.cpp
@@ -0,0 +1,13 @@
+// Progr1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World!!!");
+	getch();
+	return 0;
+}
+
